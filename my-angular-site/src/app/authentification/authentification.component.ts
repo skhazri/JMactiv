e68457b4e40df5187f3a45af378511859d183d95
skhazri <khazri.sarah@gmail.com>
2019-03-13 15:56:31 -0400

    adds authentification component

--- my-angular-site/src/app/authentification/authentification.component.ts
+++ my-angular-site/src/app/authentification/authentification.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, ParamMap, Router} from "@angular/router";
-import {map, switchMap} from "rxjs/operators";
-import {Observable} from "rxjs";
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { map, switchMap } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { AuthService, SocialUser } from 'angular-6-social-login';
+import { FacebookService } from '../services/facebook.service';
 
 @Component({
   selector: 'app-authentification',
@@ -10,19 +12,44 @@ import {Observable} from "rxjs";
 })
 export class AuthentificationComponent implements OnInit {
   public name: string;
-  public email: string;
-  constructor( private route: ActivatedRoute,
-               private router: Router) {
+  user: SocialUser;
+  loggedIn: boolean;
+  data: [];
+  events: [];
+  eventsLenght: number;
 
-    console.log(this.name);
-  }
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private socialAuthService: AuthService,
+    private facebookService: FacebookService
+  ) {}
+
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.name = params["name"];
-      this.email = params["email"];
-      console.log( "hello"+this.name);
     });
+    this.socialAuthService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+    this.getEvents();
+
   }
-
-
+  /**
+   * 
+   */
+  public signOut() {
+    this.socialAuthService.signOut();
+    this.router.navigate(['']);
+  }
+  /**
+   * get FB events associated to user 
+   */
+  public getEvents() {
+    this.facebookService.getEvents(this.user.id, this.user.token)
+      .subscribe((data) => {
+        this.events = (data.json().data);
+        console.log(this.events);
+      });
+  }
 }
