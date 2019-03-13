a9b311f64ff944a45f9831693f3e82dbdbcbde3b
skhazri <khazri.sarah@gmail.com>
2019-03-13 16:02:50 -0400

    update modules

--- my-angular-site/src/app/app.module.ts
+++ my-angular-site/src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { AppRouting } from './app.routing';
 
-import {SocialLoginModule, FacebookLoginProvider, AuthServiceConfig} from "angular-6-social-login";
+import { SocialLoginModule, FacebookLoginProvider, AuthServiceConfig } from 'angular-6-social-login';
 import { LoginComponent } from './login/login.component';
-
 import { getAuthServiceConfigs } from "../../facebookConfig";
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { ChercherComponent } from './chercher/chercher.component';
 import { CreeractiviteComponent } from './creeractivite/creeractivite.component';
-import {AppRouting} from "./app.routing";
 import { AccueilComponent } from './accueil/accueil.component';
+import { FacebookService } from './services/facebook.service'
+import { AfficheractiviteComponent } from './afficheractivite/afficheractivite.component';
+
 
 //Decorator
 @NgModule({
@@ -22,19 +24,24 @@ import { AccueilComponent } from './accueil/accueil.component';
     ChercherComponent,
     CreeractiviteComponent,
     AccueilComponent,
+    AfficheractiviteComponent,
+
 
   ],
   imports: [
     BrowserModule,
     SocialLoginModule,
-    AppRouting
+    AppRouting,
+    HttpModule
   ],
   providers: [
     {
       provide: AuthServiceConfig,
-      useFactory: getAuthServiceConfigs
-    }
+      useFactory: getAuthServiceConfigs,
+    },
+    FacebookService
   ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
