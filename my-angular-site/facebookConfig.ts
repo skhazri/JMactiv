adad253178a4c858d992f010a2e59b94109b52f1
skhazri <khazri.sarah@gmail.com>
2019-03-13 15:59:42 -0400

    add facebook configuration

--- my-angular-site/facebookConfig.ts
+++ my-angular-site/facebookConfig.ts
@@ -1,12 +1,13 @@
-// Configs
-import {AuthServiceConfig, FacebookLoginProvider} from "angular-6-social-login";
+// Facebook Configs
+import { AuthServiceConfig, FacebookLoginProvider } from 'angular-6-social-login';
 
 export function getAuthServiceConfigs() {
+    let appID = "2229942433740406";
     let config = new AuthServiceConfig(
         [
             {
                 id: FacebookLoginProvider.PROVIDER_ID,
-                provider: new FacebookLoginProvider("2229942433740406")
+                provider: new FacebookLoginProvider(appID)
             }
         ]
     );
