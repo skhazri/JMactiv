54692087ca3ea0a66304afe96777d8ff80009739
skhazri <khazri.sarah@gmail.com>
2019-03-13 15:57:39 -0400

    adds login component

--- my-angular-site/src/app/login/login.component.ts
+++ my-angular-site/src/app/login/login.component.ts
@@ -1,42 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationExtras } from "@angular/router";
-import {
-  AuthService,
-  FacebookLoginProvider,
-} from 'angular-6-social-login';
+import { Router, NavigationExtras } from '@angular/router';
+import { AuthService, FacebookLoginProvider, SocialUser } from 'angular-6-social-login';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   name: string;
-  email: string;
+  user: SocialUser;
+  loggedIn: boolean = false;
+  constructor(
+    private router: Router,
+    private socialAuthService: AuthService) { }
 
-  constructor( private socialAuthService: AuthService, private router : Router) { }
-
+  ngOnInit() {
+    this.socialAuthService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
   public socialLogin() {
-    let socialPlatformProvider;
-
-
-    socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+    let socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
     this.socialAuthService.signIn(socialPlatformProvider).then(
-        (userData) => {
-          this.name = userData.name;
-          this.email = userData.email;
-          console.log("facebook sign in data : ", this.name);
-            let navigationExtras: NavigationExtras = {
-                queryParams: {
-                    name: this.name,
-                     email: this.email
-                }
-            };
-          this.router.navigate(['toBeNamed'],navigationExtras);
-        }
+      (userData) => {
+        this.name = userData.name;
+        let navigationExtras: NavigationExtras = {
+          queryParams: {
+            name: this.name
+          }
+        };
+        this.router.navigate(['profile'], navigationExtras);
+      }
     );
   }
-  ngOnInit() {
-  }
-
 }
