bdd14041f671bddc12fe3faf7926d453199699fe
skhazri <khazri.sarah@gmail.com>
2019-03-13 15:58:45 -0400

    adds auth guard

--- my-angular-site/src/app/auth/auth.guard.ts
+++ my-angular-site/src/app/auth/auth.guard.ts
@@ -1,18 +1,34 @@
+/**
+ *  Angular’s route guards are interfaces which can tell the router whether
+ *  or not it should allow navigation to a requested route.
+ */
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router, UrlTree} from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService, FacebookLoginProvider, SocialUser } from 'angular-6-social-login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  user: SocialUser;
+  loggedIn: boolean;
+  constructor(private socialAuthService: AuthService, private router: Router) { }
 
-  constructor(private router: Router){};
-  canActivate(
-      next: ActivatedRouteSnapshot,
-      state: RouterStateSnapshot): boolean {
-    console.log('AuthGuard#canActivate called');
-    this.router.navigate(['/toBeNamed']);
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    // check if the user is logged in or not
+    //You receive a SocialUser object when the user logs in and a null when the user logs out
+    this.socialAuthService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+
+    if (this.loggedIn) {
+      return true;
+    }
+
+    // // navigate to home page
+    this.router.navigate(['']);
     return false;
   }
 }
