70406d91809723dca4f8b6390fc4ed5e12174847
skhazri <khazri.sarah@gmail.com>
2019-03-17 20:47:45 -0400

    add share component

---
+++ src/app/share/share.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+library.add(faFacebook);
+
+@Component({
+  selector: 'app-share',
+  templateUrl: './share.component.html',
+  styleUrls: ['./share.component.scss']
+})
+export class ShareComponent implements OnInit {
+  currentUrl: String;
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    // get current url
+    this.currentUrl = "http://www.google.com/" + this.router.url;
+
+    //uncomment when depolyment
+    //this.currentUrl = window.location.href;
+  }
+
+}
