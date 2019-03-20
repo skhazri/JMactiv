49e47c767f2d3b4f3059a9b61eaa9658c730a909
skhazri <khazri.sarah@gmail.com>
2019-03-20 02:18:33 -0400

    Deplacement du tableau des activites

--- my-angular-site/src/app/malisteactivite/malisteactivite.component.ts
+++ my-angular-site/src/app/malisteactivite/malisteactivite.component.ts
@@ -1,7 +1,7 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { JmactiveactivitesService } from "../jmactiveactivites.service";
 import { oracledb } from "oracledb";
-declare var $;
+//declare var $;
 
 @Component({
   selector: 'app-malisteactivite',
@@ -13,38 +13,38 @@ export class MalisteactiviteComponent implements OnInit {
   activites: any;
   activite: Activities  = new Activities();
 
-  @ViewChild('dataTable') table: ElementRef;
-  dataTable: any;
-  dtOption: any;
+  //@ViewChild('dataTable') table: ElementRef;
+  //dataTable: any;
+  //dtOption: any;
   //tableActivite: any = oracledb.activite;
 
   constructor(private activiteService: JmactiveactivitesService) { }
 
+
   ngOnInit() {
-    this.dataTable = $(this.table.nativeElement);
-    this.dataTable.dataTable(this.dtOption);
-    /*
-    this.dtOption = {
+    this.getActivites();
+    //on a besoin d'un chemin pour prendre injecter les données dans la table
+/*    this.dtOption = {
         "paging":   true,
         "ordering": true,
         "info":     true
     };
 
 
+
     this.dataTable = $(this.table.nativeElement);
     this.dataTable.dataTable(this.dtOption);
-    */
-    this.getActivites();
+*/
+
     //this.activiteService.get();
   }
 
-
+  //il fait quoi lui? Il va chercher les données de la table ACTIVITIES
   getActivites(){
     this.activiteService.get().subscribe(res => {
       this.activites = res;
       console.log(this.activites);
       console.log("getActivites accueil")
-
     },error => {
       console.log(error);
     });
