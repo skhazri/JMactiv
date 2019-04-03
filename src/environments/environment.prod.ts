0f0be4453c6ff17ba9846a3f3927014d0bf87cbb
skhazri <khazri.sarah@gmail.com>
2019-04-03 16:29:31 -0400

    build for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   Facebook_API: "https://graph.facebook.com/v3.2",
   App_ID: "2229942433740406",
   GOOGLE_MAPS_API_KEY:"AIzaSyCkCFTN_v4H8T3BjrU-76w_w1MFSLWfjqw",
-  ACTIVITY_API:"zeta2.labunix.uqam.ca"
+  ACTIVITY_API:"http://localhost:3000"
 };
