d05164f5492b7794880aba1a86189c48a60efb63
skhazri <khazri.sarah@gmail.com>
2019-04-14 01:32:02 -0400

    remove ordb dependency

--- server/server.js
+++ server/server.js
@@ -361,6 +361,7 @@ app.post("/api/DeleteActivity", function (req, res) {
         });
 });
 
-app.listen(process.env.PORT || 3000 , function () {
-    console.log('listen port 3000')
+app.listen(process.env.PORT || 3000 , function (res) {
+    console.log('listen port 3000');
+    res.send('server is running');
 });
