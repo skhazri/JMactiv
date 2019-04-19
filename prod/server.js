3b8467231ca4b71ddad72cbb492a1000862c1df4
skhazri <khazri.sarah@gmail.com>
2019-04-19 17:28:59 -0400

    update path

--- prod/server.js
+++ prod/server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '../../dist/my-angular-site'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/my-angular-site/index.html'));
+    res.sendFile(path.join(__dirname + '../../dist/my-angular-site/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
